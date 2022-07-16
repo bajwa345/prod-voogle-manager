ae2aeb1b6eb57021de46b45fe5bb9fa832cbcaba
Usman Mehmood <bajwa345@gmail.com>
2022-07-17 00:07:14 +0500

    envs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,4 @@
 export const environment = {
     production: true,
-<<<<<<< HEAD
-    apiUrl : 'http://api.voogleapp.com/api' //'http://localhost:5001/api'//'https://voogle.azurewebsites.net/api' //
-=======
-    apiUrl : 'https://voogle.azurewebsites.net/api' 
->>>>>>> fdd041c74f7fe6d5dcc11de801edcd6150e953ca
+    apiUrl : 'https://voogle.azurewebsites.net/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,11 +4,7 @@
 
 export const environment = {
     production: false,
-<<<<<<< HEAD
-    apiUrl : 'http://api.voogleapp.com/api' //'http://localhost:5001/api' //'https://voogle.azurewebsites.net/api' //'http://localhost:5001/api'
-=======
     apiUrl : 'https://voogle.azurewebsites.net/api'
->>>>>>> fdd041c74f7fe6d5dcc11de801edcd6150e953ca
 };
 
 /*
